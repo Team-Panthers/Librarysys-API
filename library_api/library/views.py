6e082242dcfe2d60643fe2d31ffa103f9dd08679
Nathan-Yinka <oludarenathaniel@gmail.com>
2024-05-30 18:22:08 +0100

    views:refactored the library views

--- library_api/library/views.py
+++ library_api/library/views.py
@@ -50,51 +50,6 @@ class LibraryReturnBookCopyApiView(LibraryAdminCreateView):
             return RackSerializer(instance)
         return StorageSerializer(instance)
 
-class LibraryAvailableBookList(BookSearchMixin,LibraryListView):
-    serializer_class = BookSerializer
-
-    def get_queryset(self):
-        library = self.library
-        return book_service.book_available(library)
-
-class LibraryAllBookList(BookSearchMixin,LibraryListView):
-    serializer_class = BookSerializer
-
-    def get_queryset(self):
-        library = self.library
-        qs = book_service.all_book_available(library)
-        return qs
-
-
-class LibraryAllBookCopies(BookCopySearchMixin,LibraryListView):
-    serializer_class = BookCopySerializer
-    
-    def get_queryset(self):
-        library = self.library
-        return book_copy_service.all_book_copy_available(library)
-    
-class LibraryAvailableBookCopies(BookCopySearchMixin,LibraryListView):
-    serializer_class = BookCopySerializer
-
-    def get_queryset(self):
-        library = self.library
-        return book_copy_service.book_copy_available(library)
-
-class LibraryBookGetBookCopies(LibraryRetrieveView):
-    serializer_class = BookCopySerializer
-    obj_error_message = "Book Not Found in the Library"
-
-    
-    def get_object_queryset(self):
-        library = self.library
-        qs = book_service.all_book_available(library)
-        return qs
-
-    def get_queryset(self):
-        library = self.library
-        book = self.get_object()
-        return book_copy_service.all_copies_for_book(library,book)
-    
 class LibraryUsersList(LibraryAdminPermissionMixin,LibraryContextMixin,LibraryListView):
     serializer_class = UserSerializer
 
